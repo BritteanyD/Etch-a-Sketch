11e02ff2a83a311945a7112dcee85c89b9d0ac27
Britteany <miszbritteany@gmail.com>
2023-09-22 23:33:03 -0400

    Centered button and title

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 const btn = document.querySelector('button');
 btn.addEventListener('click', () => {
     prompt('Enter your board size by choosing a number between 1 and 50');
+    if (size >= 1 && size <= 50) {
+        createGrid(size);
+    } else {
+        alert('Please enter a valid size between 1 and 50');
+    }
 });
 
-const container = document.getElementById('container');
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const grid = document.createElement('div');
-        grid.classList.add('grid');
-        grid.style.width = '20px';
-        grid.style.height = '20px';
-        grid.style['background-color'] = 'red';
-        grid.style.border = 'solid';
-        grid.style.display = 'flex';
+function createGrid(size) {
+    const container = document.getElementById('container');
+    container.innerHTML = '';
+    container.style.gridTemplateColumns = 'repeat(${size}, 1fr)';
+    container.style.gridTemplateRows = 'repeat(${size}, 1fr)';
 
-        container.appendChild(grid);
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const grid = document.createElement('div');
+            grid.classList.add('grid');
+            grid.style.width = '20px';
+            grid.style.height = '20px';
+            grid.style['background-color'] = 'red';
+            grid.style.border = 'solid';
+            grid.style.display = 'flex';
 
-        grid.addEventListener('mouseover', () => {
-            grid.style['background-color'] = 'blue';
-        });
+            container.appendChild(grid);
 
+            grid.addEventListener('mouseover', () => {
+                grid.style['background-color'] = 'blue';
+            });
+
+        }
     }
 }
+createGrid(16);
